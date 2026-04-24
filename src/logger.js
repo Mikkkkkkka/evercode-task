bc90dde7a1b49dc3d2acb87cc35f622387323582
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-04-25 01:11:26 +0300

    fix: timestamp in the logger func

--- src/logger.js
+++ src/logger.js
@@ -1,8 +1,21 @@
 import config from "./config.js";
 
+function formatDate(date) {
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = String(date.getFullYear());
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  return `${day}.${month}.${year}-${hours}:${minutes}:${seconds}`;
+}
+
 function createLogger(scope = config.appName) {
   return function log(message) {
-    console.log(`[${scope}] ${Date.now()} -- ${message}`);
+    const timestamp = formatDate(new Date());
+
+    console.log(`[${scope}] [${timestamp}] -- ${message}`);
   };
 }
 
